b3a8f019787a62221206652348c2640274be3a3d
Sergey Saprankov <sergey-saprankov@mail.ru>
2022-12-04 15:08:49 +0300

    todo redux, added new content

---
+++ src/Task/Task.tsx
@@ -0,0 +1,65 @@
+import React, {ChangeEvent, useCallback} from "react";
+import s from "../TodoLIst/TodoList.module.css";
+import {TextArea} from "../EditableSpan/TextArea";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import {Checkbox} from "@mui/material";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import {TaskType} from "../redux/task-reducer";
+
+type TaskTypeProps = {
+    task: TaskType
+    todoListId: string
+    taskId: string
+    deleteTask: (todoListId: string, taskId: string) => void
+    onChangeCheckbox: (todoListId: string, taskId: string, value: boolean) => void
+    onChangeTaskPriority: (priority: string, todoListId: string, taskId: string, isDone: boolean) => void
+    changeTaskTitle: (todoListId: string,  taskId: string, taskTitle: string) => void
+}
+
+export const Task: React.FC<TaskTypeProps> = React.memo( ({task,taskId,deleteTask, onChangeCheckbox, changeTaskTitle, onChangeTaskPriority, todoListId}) => {
+    const changeTaskTitleByTextArea = useCallback( (value: string) => {
+        changeTaskTitle(todoListId, taskId, value)
+    }, [todoListId, taskId, changeTaskTitle])
+    const onChangePriority = () => {
+        if (!task.isDone) {
+            onChangeTaskPriority(task.priority, todoListId, taskId, task.isDone);
+        }
+    };
+
+    const deleteTaskHandler = () => {
+        deleteTask(todoListId, taskId)
+    }
+    const onChangeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const isDone = e.currentTarget.checked
+        onChangeCheckbox(todoListId, taskId, isDone)
+    }
+    return (
+        <div
+            className={!task.isDone ? `${s.taskContainer} ${s[task.priority]}` : `${s.taskContainer} ${s.isDone}`}
+            key={task.taskId}
+        >
+            <div className={s.taskWrapper}>
+                <TextArea callBack={changeTaskTitleByTextArea}
+                          placeholder={'Task title'}
+                          title={task.taskTitle}/>
+                <div className={s.wrapperPriority}>
+                    <div onClick={onChangePriority} className={s.priorityText}>
+                        <div>{task.priority}</div>
+                        <ArrowBackIcon fontSize={'small'}/>
+                    </div>
+                </div>
+            </div>
+            <div className={s.checkboxContainer}>
+                <Checkbox sx={{
+                    color: '#fff',
+                    '&.Mui-checked': {
+                        color: "#dce775",
+                    },
+                }} onChange={onChangeCheckboxHandler} checked={task.isDone}/>
+            </div>
+            <div className={s.clearContainer}>
+                <HighlightOffIcon onClick={deleteTaskHandler}/>
+            </div>
+        </div>
+    )
+})
--- src/TodoLIst/TodoList.tsx
+++ src/TodoLIst/TodoList.tsx
@@ -1,22 +1,13 @@
-import React, {ChangeEvent, useCallback} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {changeFilterTodoListAC, FilterTaskType, TodoListType} from "../redux/todoList-reducer";
+import React, {useCallback} from "react";
+import {FilterTaskType} from "../redux/todoList-reducer";
 import s from "./TodoList.module.css";
-import {StoreType} from "../redux/store";
-import {
-    addTaskAC,
-    TaskStateType,
-    changeTaskPriorityAC, deleteTaskAC, taskIsDoneAC, sortTaskAC, TaskType,
-} from "../redux/task-reducer";
+import {TaskType,} from "../redux/task-reducer";
 import {Button, Checkbox, Stack} from "@mui/material"
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import AddIcon from '@mui/icons-material/Add';
-import SortIcon from '@mui/icons-material/Sort';
-import SettingsIcon from '@mui/icons-material/Settings';
 import {Filter} from "../Filter/Filter";
-import {logDOM} from "@testing-library/react";
 import {TextArea} from "../EditableSpan/TextArea";
+import {Task} from "../Task/Task";
 
 type TodoListPropsType = {
     tasks: TaskType[]
@@ -31,7 +22,7 @@ type TodoListPropsType = {
     onChangeCheckbox: (todoListId: string, taskId: string, value: boolean) => void
     deleteTodoList: (todoListId: string) => void
     changeTodoListTitle: (todoListId: string, todoListTitle: string) => void
-    changeTaskTitle: (todoListId: string,  taskId: string, taskTitle: string) => void
+    changeTaskTitle: (todoListId: string, taskId: string, taskTitle: string) => void
 }
 
 
@@ -59,57 +50,27 @@ export const TodoList: React.FC<TodoListPropsType> = React.memo(({
     const removeTodoListHandler = () => {
         deleteTodoList(todoListId)
     }
-    const sortTasksByFilter = useCallback(() => sortTasks(todoListId), [todoListId])
+    const sortTasksByFilter = useCallback(() => sortTasks(todoListId), [todoListId, sortTasks])
     const changeFilterByFilter = useCallback((filter: FilterTaskType) => {
         changeFilterTodoList(todoListId, filter)
-    }, [todoListId, filter])
+    }, [todoListId, filter, changeFilterTodoList])
     const changeTodoListTitleByTextArea = useCallback((value: string) => {
         changeTodoListTitle(todoListId, value)
-    }, [todoListId])
+    }, [todoListId, changeTodoListTitle])
 
 
-    const tasksMap = filteredTasks?.map((task) => {
-        const changeTaskTitleByTextArea = (value: string) => {
-            changeTaskTitle(todoListId, task.taskId, value)
-        }
-        const onChangePriority = () => {
-            if (!task.isDone) {
-                onChangeTaskPriority(task.priority, todoListId, task.taskId, task.isDone);
-            }
-        };
-        const deleteTaskHandler = () => {
-            deleteTask(todoListId, task.taskId)
-        }
-        const onChangeCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            const isDone = e.currentTarget.checked
-            onChangeCheckbox(todoListId, task.taskId, isDone)
-        }
+    const tasksMap = filteredTasks?.map((t) => {
+
         return (
-            <div
-                className={!task.isDone ? `${s.taskContainer} ${s[task.priority]}` : `${s.taskContainer} ${s.isDone}`}
-                key={task.taskId}
-            >
-                <div className={s.taskWrapper}>
-                    <TextArea callBack={changeTaskTitleByTextArea}  placeholder={'Task title'} title={task.taskTitle}/>
-                    <div className={s.wrapperPriority}>
-                        <div onClick={onChangePriority} className={s.priorityText}>
-                            <div>{task.priority}</div>
-                            <ArrowBackIcon fontSize={'small'}/>
-                        </div>
-                    </div>
-                </div>
-                <div className={s.checkboxContainer}>
-                    <Checkbox sx={{
-                        color: '#fff',
-                        '&.Mui-checked': {
-                            color: "#dce775",
-                        },
-                    }} onChange={onChangeCheckboxHandler} checked={task.isDone}/>
-                </div>
-                <div className={s.clearContainer}>
-                    <HighlightOffIcon onClick={deleteTaskHandler}/>
-                </div>
-            </div>
+            <Task task={t}
+                  todoListId={todoListId}
+                  taskId={t.taskId}
+                  deleteTask={deleteTask}
+                  onChangeCheckbox={onChangeCheckbox}
+                  onChangeTaskPriority={onChangeTaskPriority}
+                  changeTaskTitle={changeTaskTitle}
+                  key={t.taskId}
+            />
         );
     });
 
@@ -125,9 +86,13 @@ export const TodoList: React.FC<TodoListPropsType> = React.memo(({
             <div className={s.wrapper}>
                 <div className={s.todoListTitleContainer}>
                     {/*<div className={s.title}>{todoListTitle}</div>*/}
-                    <TextArea callBack={changeTodoListTitleByTextArea} placeholder={'Todo List title'} title={todoListTitle}/>
-                    <Filter changeFilterByFilter={changeFilterByFilter} sortTasksByFilter={sortTasksByFilter}
-                            filter={filter} todoListId={todoListId}/>
+                    <TextArea callBack={changeTodoListTitleByTextArea}
+                              placeholder={'Todo List title'}
+                              title={todoListTitle}/>
+                    <Filter
+                        changeFilterByFilter={changeFilterByFilter}
+                        sortTasksByFilter={sortTasksByFilter}
+                        filter={filter} todoListId={todoListId}/>
                 </div>
                 <div className={s.tasksContainer}>{tasksMap}</div>
                 <Button
